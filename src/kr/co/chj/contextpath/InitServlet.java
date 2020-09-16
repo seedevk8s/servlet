37cd6b7424d532e24c73deac6b3cb560ef039b27
seedevk8s <seedevk8s@gmail.com>
2020-09-16 23:11:23 +0900

    서블릿에 대한 초기화 작업

---
+++ src/kr/co/chj/contextpath/InitServlet.java
@@ -0,0 +1,47 @@
+package kr.co.chj.contextpath;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InitServlet
+ */
+//@WebServlet("/Init")
+public class InitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InitServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//ServletConfig
+		String id = getInitParameter("id");
+		String pw = getInitParameter("pw");
+		String local = getInitParameter("local");
+		
+		System.out.println("id : " + id);
+		System.out.println("비밀번호 : " + pw);
+		System.out.println("지역 : " + local);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
