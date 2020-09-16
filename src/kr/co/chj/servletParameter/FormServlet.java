1300bf60cac599a4e650fa6571fa09ffa8a615c2
seedevk8s <seedevk8s@gmail.com>
2020-09-16 10:29:39 +0900

    getParameter()를 이용해 <input>태그의 value()값 받아오기

--- src/kr/co/chj/servletParameter/FormServlet.java
+++ src/kr/co/chj/servletParameter/FormServlet.java
@@ -56,8 +56,52 @@ public class FormServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		/*
+		 * <한글처리>
+		 * 1.톰캣서버의 default 문자 처리 방식 : ISO-8859-1 방식임.
+		 * 2.서블릿단에서 별도의 한글 인코딩을 하지 않으면 한글이 깨질수 있음.
+		 * 3.get 방식과 post방식의 한글처리가 다름.
+		 * 4.get 방식 한글처리 : Servers(톰캣서버 폴더내)에 있는 server.xml에서 
+		 * 					<connector>에 URIEncoding="UTF-8" 추가해야함.
+		 * 5.post 방식 한글처리 : doPost() 메서드 내에서 설정함
+		 * 					 request.setCharacterEncoding("UTF-8");
+		 */
+		request.setCharacterEncoding("utf-8");	//전송된 데이터를 UTF-8으로 인코딩함.
+		
+		//getParameter()를 이용해 <input>태그의 name 속성값으로 전송된 value값을 받아 옴.
+		String name = request.getParameter("name");
+		String id = request.getParameter("id");
+		String password = request.getParameter("pw");
+		
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
