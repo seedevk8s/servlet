6ae6614bf94a29d7cc76c42df828eb70e13fcdfb
seedevk8s <seedevk8s@gmail.com>
2020-09-18 07:00:11 +0900

    ServletConfig 서블릿에 대한 초기화 작업 기능2

--- src/kr/co/chj/contextpath/InitServlet.java
+++ src/kr/co/chj/contextpath/InitServlet.java
@@ -2,6 +2,7 @@ package kr.co.chj.contextpath;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,7 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class InitServlet
  */
-//@WebServlet("/Init")
+@WebServlet(urlPatterns = {"/Init", "/Init2"},   //urlPatterns를 이용해 매핑 이름을 여러 개 설정할수 있음.
+			initParams = {@WebInitParam(name="id", value="admin2"),		//@WebInitParam을 이용해 여러 개의 매개변수를 설정.
+						  @WebInitParam(name="pw", value ="12345"),
+						  @WebInitParam(name="local", value ="Inchon")})
 public class InitServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +31,7 @@ public class InitServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//ServletConfig
-		String id = getInitParameter("id");
+		String id = getInitParameter("id");		//설정한 매개변수의 name으로 값을 가져옴.
 		String pw = getInitParameter("pw");
 		String local = getInitParameter("local");
 		
